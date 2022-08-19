d2aa159b9dbe777b124c6ed7fd7964261283fa9f
mahendarreddychilumula <mahendarreddy.chilumula@gmail.com>
2022-08-19 13:17:06 +0530

    rg_vnet_subnet

---
+++ powershell.ps1
@@ -0,0 +1,25 @@
+$resorce_group = @{ 
+name = 'mahendar'
+location = 'eastus'
+}
+New-AzResourceGroup @resorce_group
+$vnet = @{
+ Name = 'mahi_vnet'
+ ResourceGroupName = $resorce_group.name
+ Location = $resorce_group.location
+ Addressprefix = '10.10.0.0/16'
+}
+$VirtualNetwork = New-AZVirtualNetwork @vnet
+$subnet = @{
+    Name = 'mahi_subnet'
+    VirtualNetwork = $VirtualNetwork
+    AddressPrefix = '10.10.0.0/24'
+}
+$subnetconfig = Add-AzVirtualNetworkSubnetConfig @subnet
+Remove-AzResourceGroup -name mahendar -Force
+
+##if (!(Get-AzResourceGroup mahendar -ErrorAction SilentlyContinue))
+  ##{ New-AzResourceGroup @resorce_group }
+##else
+  ## {"found"} 
+   
